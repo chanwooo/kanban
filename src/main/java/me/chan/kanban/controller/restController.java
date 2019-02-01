20274aac0b6c8fa9ab7d2f5f42b2f55a33cf65b1
chanwooo <chanwookim@me.com>
2019-02-01 19:23:14 +0900

    modify test controller

--- src/main/java/me/chan/kanban/controller/restController.java
+++ src/main/java/me/chan/kanban/controller/restController.java
@@ -1,15 +1,25 @@
 package me.chan.kanban.controller;
 
 import me.chan.kanban.domain.Member;
+import me.chan.kanban.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class restController {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
     @GetMapping("/user")
-    public String userInfo(Member user)
+    public String userInfo(Principal principal)
     {
-        return user.toString();
+        Member member = memberRepository.findByEmail(principal.getName());
+        return "Hi "+member;
     }
 
 }
